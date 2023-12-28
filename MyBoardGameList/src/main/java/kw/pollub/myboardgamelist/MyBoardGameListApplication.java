1f76fdcb24092e88992c5c8be3cefeec42481134
Klizlo <klizlo0111@gmail.com>
2023-12-28 15:04:18 +0100

    Init commit
    Create backend

---
+++ MyBoardGameList/src/main/java/kw/pollub/myboardgamelist/MyBoardGameListApplication.java
@@ -0,0 +1,13 @@
+package kw.pollub.myboardgamelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBoardGameListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBoardGameListApplication.class, args);
+    }
+
+}
---
+++ MyBoardGameList/src/test/java/kw/pollub/myboardgamelist/MyBoardGameListApplicationTests.java
@@ -0,0 +1,13 @@
+package kw.pollub.myboardgamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBoardGameListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
